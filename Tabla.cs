189bb9df9ddd81317c1105ea4745e033ae01bb95
Josue Garcia <68764971+JosueDev-spec@users.noreply.github.com>
2020-12-09 15:32:01 -0600

    Add files via upload

---
+++ Tabla.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace QuickSharp
+{
+    public class Tabla{
+        private int n;
+        public Tabla(int n){
+            this.n = n;
+       }
+       public void mostarTb(){
+           
+        for(int i= 0 ;i<11;i++){
+            int r = n*i;
+            Console.WriteLine(i+"*"+n+" = "+r);
+    }
+       }
+    }
+    public class Program
+    {
+        private static void Main()
+        {
+           
+            Tabla tb = new Tabla(3);
+            tb.mostarTb();
+            
+            Console.ReadKey();
+        }
+    }
+}
